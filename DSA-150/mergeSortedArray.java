885d783be694ef068e7aca16dbac7364d5d1caff
vekixkami <vekix77@gmail.com>
2026-03-17 10:48:47 +0530

    new changes

---
+++ DSA-150/mergeSortedArray.java
@@ -0,0 +1,18 @@
+class mergeSortedArray {   
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+    public static void main(String[] args){
+        int[] nums1={1,2,3,0,0,0};
+        int[] nums2={2,5,6};
+        int m=3;
+        int n=3;
+        merge(nums1,m,nums2,n);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
+
